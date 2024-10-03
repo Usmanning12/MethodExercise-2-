8503a8aac9a65cf0fc1517ed781bdc47f3e7714b
Darron Manning <darronmanning@yahoo.com>
2024-10-02 17:15:25 -0700

    completed exercise 2 with math operation methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
+
+            Console.WriteLine(Add(2, 4));
+            Console.WriteLine(Sub(13, 500));
+            Console.WriteLine(Mult(12, 12));
+            Console.WriteLine(Div(144, 12));
+        }
+
+
+
+
+
+
+
+
+
+
+        public static void MadLib()
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello,{userName}! what is your color?");
+            string favColor = Console.ReadLine();
+            Console.WriteLine("Favorite band?");
+            string FavBand = Console.ReadLine();
+            Console.WriteLine("What is your favorite Animal?");
+            string FavAnimal = Console.ReadLine();
+            Console.WriteLine(
+                $"One day, Jamimah and {userName} was walking through the woods while wearing a {favColor} t-shirt. They saw a {FavAnimal} dancing to the {FavBand} and they asked if they could join the {FavAnimal} to dance.");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b; 
+        }
+
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a / b;
         }
     }
-}
+    
+    
+
+
+}    
+      
